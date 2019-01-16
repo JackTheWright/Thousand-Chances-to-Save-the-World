236f2192ac290072f6f8432221ce4eaff90c2b15
Jack Wright <jackwright@Jacks-MBP.vs.shawcable.net>
2019-01-16 10:19:49 -0800

    All project files up to Jan 16 2019
    -Map done including detailing
    -Mushroom and earth enemies done (few small bugs on earth ones)
    -Player basics are done
    -UI has been started
    -Still need weapon / item system
    -Need to finish health system
    -Need to add main menu and end screen
    -Need to add pause menu
    -Need to add deaths (player and enemy)
    -Need to add one more enemy type

---
+++ A Thousand Chances to Save the World!/Assets/scripts/EarthBaddy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthBaddy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    public Rigidbody2D myRigidBody;
+    public Transform target;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) < range)
+        {
+
+            if (transform.position.x - target.position.x > 0)
+            {
+                print("self position is > player");
+                //spriterenderer.flipX = true;
+                transform.localScale = new Vector3(-1, 1, 1);
+
+            }
+            else if (transform.position.x - target.position.x < 0)
+            {
+                print("self position is < player");
+                //spriterenderer.flipX = false;
+                transform.localScale = new Vector3(1, 1, 1);
+
+            }
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), moveSpeed * Time.deltaTime + .01f);
+
+
+
+
+            Debug.Log("I see you bitch.\n");
+        }
+        else if (IsFacingRight() && (Vector3.Distance(target.position, transform.position) >= range))
+        {
+            myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else if (!IsFacingRight() && (Vector3.Distance(target.position, transform.position) >= range))
+        {
+            myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+    }
+
+
+}   
+
---
+++ A Thousand Chances to Save the World!/Assets/scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour {
+    Rigidbody2D myrigidbody;
+    Animator myanimator;
+    Collider2D mycollider;
+    CapsuleCollider2D mybodycollider;
+    BoxCollider2D myfeetcollider;
+
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float runspeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
+
+    bool isAlive = true;
+    int HP;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = GetComponent<Rigidbody2D>();
+        myanimator = GetComponent<Animator>();
+        mybodycollider = GetComponent<CapsuleCollider2D>();
+        myfeetcollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isAlive) { return; }
+        Run();
+        FlipSprite();
+        Jump();
+    	}
+
+    private void Run () {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 velocity = new Vector2(controlThrow * runspeed, myrigidbody.velocity.y);
+        myrigidbody.velocity = velocity;
+
+        if (Mathf.Abs(myrigidbody.velocity.x) > Mathf.Epsilon){
+            myanimator.SetBool("Running", true);
+        }
+        else {
+            myanimator.SetBool("Running", false);
+        }
+    }
+
+    private void Jump() {
+        if (!myfeetcollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        
+        if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+            Vector2 jumpUp = new Vector2(0f, jumpSpeed);
+            myrigidbody.velocity += jumpUp;
+        }
+    }
+
+    private void FlipSprite() {
+
+        bool playerHorizonVelocity = Mathf.Abs(myrigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHorizonVelocity) {
+            transform.localScale = new Vector2((float)(Mathf.Sign(myrigidbody.velocity.x) * 2.657), (float)2.355);
+        }
+    }
+
+    private void loseHP()
+    {
+        if (mybodycollider.IsTouchingLayers(LayerMask.GetMask("Enemy"))) {
+            HP -= 1;
+            if (HP == 0)
+            {
+                myanimator.SetTrigger("Die");
+                GetComponent<Rigidbody2D>().velocity = deathKick;
+            }
+        }
+    }
+
+}
